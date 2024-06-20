b9223dc4e148d08ca2d5cdfe36d4975bddf523ec
Mohammed Rizwan <rizwan@truscholar.io>
2024-06-20 13:54:23 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log('Welcome to eBook APIS');
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 8080;
+
+  app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`);
+  });
+};
+
+
+startServer()
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+//Routes
+
+//Http methods: GET, POST, PUT, PATCH, DELETE
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "Welcome to elib apis" });
+});
+
+export default app;
